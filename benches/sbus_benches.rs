e10cfc028068a96435ecfcd5ae26fc6cc6317155
ragarnoy <tiagolernould@gmail.com>
2025-06-05 11:32:26 +0200

    not all features, and fmt

--- benches/sbus_benches.rs
+++ benches/sbus_benches.rs
@@ -1,10 +1,10 @@
-use std::hint::black_box;
 use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion};
 use embedded_io_adapters::std::FromStd;
 use sbus_rs::{
     pack_channels, SbusPacket, SbusParser, CHANNEL_COUNT, CHANNEL_MAX, SBUS_FOOTER,
     SBUS_FRAME_LENGTH, SBUS_HEADER,
 };
+use std::hint::black_box;
 use std::io::Cursor;
 
 const fn generate_alternating() -> [u16; CHANNEL_COUNT] {
